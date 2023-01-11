df175e062b28f4c214fe0e274eb055305cc2e04c
AGENTno6 <dhowell831@gmail.com>
2023-01-11 06:27:54 +0000

    completed tasks 1-6 successfully

--- problems/exercises.js
+++ problems/exercises.js
@@ -1,12 +1,40 @@
 // Update the starter code of this file 
 
-const cellphone = {};
+//QUESTION1
+const cellphone = {
+  phoneNumber: "/\d\d\d\d\d\d\d\d\d\d/",
+  model: ""
+  
+};
 
-// Question 11
-const makeCellPhone = function() {
-
+function makeCellPhone (phoneNumber) {
+  return{
+    phoneNumber: phoneNumber,
+    model : " ",
+    contacts: [],
+    addContact(name, phoneNumber){
+      this.contacts.push({name: name, phoneNumber: phoneNumber});
+      console.log(`${name} successfully added.`);
+    },
+    numberOfContacts() {
+      function sum (a,b){
+        let total = 0;
+        total = a + b 
+        console.log(total);
+      };
+      this.contacts.forEach(sum);
+    };
+  };
 }
 
+let myPhone = makeCellPhone(6462833862);
+myPhone.addContact("Sarah",3479829094);
+myPhone.addContact("Jake",344929624);
+myPhone.addContact("Tyler",3476429581);
+// console.log(myPhone.contacts)
+contacts.numberOfContacts
+
+/////////////////////////////////////////////////////////////
 module.exports = {
   cellphone,
   makeCellPhone,
