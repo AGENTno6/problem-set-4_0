070508c3685cb7c16bcac8b41d3e750d321606ea
AGENTno6 <dhowell831@gmail.com>
2023-01-12 05:08:45 +0000

    all tests passed

--- problems/exercises.js
+++ problems/exercises.js
@@ -2,11 +2,48 @@
 
 //QUESTION1
 const cellphone = {
-  phoneNumber: /\d{10}/,
-  model: ""
-  
+  phoneNumber: "6463823862",
+//QUESTION2
+  model: "iPhone XR",
+//QUESTION3
+  contacts: [],
+//QUESTION5  
+  addContact(name, phoneNumber){
+//QUESTION4
+    this.contacts.push({name:name ,phoneNumber: phoneNumber});
+    return `${name} successfully added.`;
+  },
+  numberOfContacts(){
+    return this.contacts.length;
+  },
+//QUESTION8
+  lookUp(name){
+    let contact = this.contacts.find(contact => contact.name === name);
+      if(!contact){
+        return "Contact not found.";
+      }else return contact.phoneNumber ;
+  },
+//QUESTION9
+  deleteContact (nameValue){
+    const contactIndex = this.contacts.findIndex(contact => contact.name === nameValue);
+    if(contactIndex === -1){
+          return "Contact not found.";
+          }else{
+            this.contacts.splice(contactIndex,1);
+            return `${nameValue} successfully deleted.`;
+        }
+  },
+//QUESTION10
+  call(nameOrNum){
+    for(let i = 0; i < this.contacts.length; i++){
+      if(nameOrNum == this.contacts[i].name || nameOrNum == this.contacts[i].phoneNumber ){
+        return `Calling ${this.contacts[i].name} at ${this.contacts[i].phoneNumber}`;
+      } 
+    }
+      return "Contact not found.";
+  }
 };
-
+//QUESTION11
 function makeCellPhone (phoneNumber, model) {
   return{
     phoneNumber: phoneNumber,
@@ -40,41 +77,35 @@ function makeCellPhone (phoneNumber, model) {
             return `${nameValue} successfully deleted.`;
         }
       },
-      call (contact){
-        this.contacts.forEach(contact => {
-          if(this.contacts.name === contact || this.contacts.phoneNumber === contact){
-            return `Calling ${contact} at ${this.contacts.phoneNumber}...`;
-          }else{
-            return "Contact not found.";
-          }
-        });
+    call (nameOrNum){
+      for(let i = 0; i < this.contacts.length; i++){
+        if(nameOrNum == this.contacts[i].name || nameOrNum == this.contacts[i].phoneNumber ){
+          return `Calling ${this.contacts[i].name} at ${this.contacts[i].phoneNumber}`;
+        } 
       }
-    };
-  }
+      return "Contact not found.";
+    }
+};
+}
 
 
-let myPhone = makeCellPhone(6462833862);
-myPhone.addContact("Sarah",3479829094);
-myPhone.addContact("Jake",344929624);
-myPhone.addContact("Tyler",3476429581);
-// console.log(myPhone.contacts)
-// console.log(myPhone.numberOfContacts());
-// console.log(myPhone.deleteContact("Tyler"));
-// console.log(this.foundContact);
-// console.log(myPhone.deleteContact("Tylers"));
-// console.log(myPhone.contacts);
-console.log(myPhone.call("Sarah"));
-
-
-
-
-
-
-
-
-
-
-
+// let myPhone = makeCellPhone(6462833862);
+// myPhone.addContact("Sarah",3479829094);
+// myPhone.addContact("Jake",344929624);
+// myPhone.addContact("Tyler",3476429581);
+// // console.log(myPhone.contacts)
+// // console.log(myPhone.numberOfContacts());
+// // console.log(myPhone.deleteContact("Tyler"));
+// // console.log(this.foundContact);
+// // console.log(myPhone.deleteContact("Tylers"));
+// // console.log(myPhone.contacts);
+// console.log(myPhone.call("Sarah"));
+// console.log(cellphone.addContact("Jessie", 7182417155));
+// console.log(cellphone.addContact("Lijah", 7182318125));
+// console.log(cellphone.addContact("Alice", 7185427145));
+// // console.log(cellphone.numberOfContacts())
+// console.log(cellphone.contacts);
+// console.log(cellphone.call("Jessie"));
 
 /////////////////////////////////////////////////////////////
 module.exports = {
